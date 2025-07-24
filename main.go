d0589be9fa7b02d372de2d4bc599e2de2de74f8e
Zane Willgruber <73185743+ZaneWillgruber@users.noreply.github.com>
2025-07-24 14:46:48 -0500

    added middleware

--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir("app/"))
-	mux.Handle("/app/", http.StripPrefix("/app/", fs))
-	mux.HandleFunc("/healthz", readinessEndpoint)
+	apiCfg := apiConfig{}
+	handler := http.StripPrefix("/app/", fs)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
+	mux.HandleFunc("GET /api/healthz", readinessEndpoint)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsEndpoint)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetMetricsEndpoint)
 
 	server := http.Server{Handler: mux, Addr: ":8080"}
 
@@ -23,4 +32,30 @@ func readinessEndpoint(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	writer.WriteHeader(200)
 	writer.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) metricsEndpoint(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/html")
+	writer.WriteHeader(200)
+	writer.Write([]byte(fmt.Sprintf(`
+		<html>
+			<body>
+				<h1>Welcome, Chirpy Admin</h1>
+				<p>Chirpy has been visited %d times!</p>
+			</body>
+		</html>
+		`, cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) resetMetricsEndpoint(writer http.ResponseWriter, request *http.Request) {
+	writer.WriteHeader(200)
+	cfg.fileserverHits.Store(0)
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		fmt.Printf("%s %s\n", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
 }
