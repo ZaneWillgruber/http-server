b0cbf699e8a23e7996e2e5e41ded1a670cbb42b2
Zane Willgruber <73185743+ZaneWillgruber@users.noreply.github.com>
2025-07-28 14:42:17 -0500

    added valid message check

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 	"sync/atomic"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
 }
+
+var badWords = [...]string{"kerfuffle", "sharbert", "fornax"}
 
 func main() {
 	mux := http.NewServeMux()
@@ -19,6 +23,7 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", readinessEndpoint)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsEndpoint)
 	mux.HandleFunc("POST /admin/reset", apiCfg.resetMetricsEndpoint)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirp)
 
 	server := http.Server{Handler: mux, Addr: ":8080"}
 
@@ -28,16 +33,16 @@ func main() {
 	}
 }
 
-func readinessEndpoint(writer http.ResponseWriter, request *http.Request) {
-	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	writer.WriteHeader(200)
-	writer.Write([]byte("OK"))
+func readinessEndpoint(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
 }
 
-func (cfg *apiConfig) metricsEndpoint(writer http.ResponseWriter, request *http.Request) {
-	writer.Header().Set("Content-Type", "text/html")
-	writer.WriteHeader(200)
-	writer.Write([]byte(fmt.Sprintf(`
+func (cfg *apiConfig) metricsEndpoint(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(200)
+	w.Write([]byte(fmt.Sprintf(`
 		<html>
 			<body>
 				<h1>Welcome, Chirpy Admin</h1>
@@ -47,8 +52,8 @@ func (cfg *apiConfig) metricsEndpoint(writer http.ResponseWriter, request *http.
 		`, cfg.fileserverHits.Load())))
 }
 
-func (cfg *apiConfig) resetMetricsEndpoint(writer http.ResponseWriter, request *http.Request) {
-	writer.WriteHeader(200)
+func (cfg *apiConfig) resetMetricsEndpoint(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
 	cfg.fileserverHits.Store(0)
 }
 
@@ -58,4 +63,85 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 		fmt.Printf("%s %s\n", r.Method, r.URL.Path)
 		next.ServeHTTP(w, r)
 	})
+}
+
+func resopondWithError(w http.ResponseWriter, code int, message string) {
+	type errorVals struct {
+		Error string `json:"error"`
+	}
+
+	respBody := errorVals{
+		Error: message,
+	}
+
+	data, err := json.Marshal(respBody)
+	if err != nil {
+		fmt.Printf("Error marshalling JSON: %s\n", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.WriteHeader(code)
+	w.Write(data)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload any) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Printf("Error marshalling JSON: %s\n", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.WriteHeader(code)
+	w.Write(data)
+}
+
+func validateChirp(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string
+	}
+
+	type response struct {
+		Body string `json:"body"`
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		fmt.Printf("Error decoding parameters: %s\n", err)
+		resopondWithError(w, 500, "something went wrong")
+		return
+	}
+
+	if params.Body == "" {
+		resopondWithError(w, 400, "no body")
+		return
+	}
+
+	if len(params.Body) > 140 {
+		resopondWithError(w, 400, "Chirp is too long")
+		return
+	}
+
+	censored := censorProfaneWords(params.Body)
+
+	respondWithJSON(w, 200, response{Body: censored})
+}
+
+func censorProfaneWords(body string) string {
+	split := strings.Split(body, " ")
+
+	for i, word := range split {
+		for _, badWord := range badWords {
+			if strings.EqualFold(word, badWord) {
+				split[i] = "****"
+			}
+		}
+	}
+
+	return strings.Join(split, " ")
 }
